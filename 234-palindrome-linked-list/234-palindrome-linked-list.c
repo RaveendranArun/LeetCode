0050e3e6a9eb11ec9357446f3131e2d84b03b9e9
Arun Raveendran <arunr4957@gmail.com>
2022-01-15 19:24:54 +0530

    Time: 152 ms (44.47%), Space: 41.2 MB (91.29%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.c
+++ 234-palindrome-linked-list/234-palindrome-linked-list.c
@@ -6,33 +6,56 @@
  * };
  */
 
-int Count(struct ListNode* p)
+struct ListNode* findFirstHalf(struct ListNode* p)
 {
-    int count = 0;
+    struct ListNode* s = p;
+    struct ListNode* f = p;
+    
+    while ((p->next != NULL) && (p->next->next) != NULL)
+    {
+        p = p->next->next;
+        s = s->next;
+    }
+    
+    return s;
+}
+
+struct ListNode* reverseSecondHalf(struct ListNode* node)
+{
+    struct ListNode* r = NULL;
+    struct ListNode* q = NULL;
+    struct ListNode* p = node;
+    
     while (p)
     {
-        ++count;
+        r = q;
+        q = p;
         p = p->next;
+        q->next = r;
     }
-    return count;
+    node = q;
+    return node;
 }
 
 bool isPalindrome(struct ListNode* head){
-    int* nums = (int* )malloc(sizeof(int) * Count(head));
-    int k = 0;
-    int i, j;
     
-    while (head)
+    if (head == NULL)
+        return true;
+    
+    struct ListNode* first = findFirstHalf(head);
+    struct ListNode* second = reverseSecondHalf(first->next);
+    
+    bool result = true;
+    struct ListNode* p = head;
+    struct ListNode* q = second;
+    while (result && q != NULL)
     {
-        nums[k++] = head->val;
-        head = head->next;
+        if (p->val != q->val)
+            result = false;
+        p = p->next;
+        q = q->next;
     }
     
-    for (i = 0, j = k-1; i < j; ++i, --j)
-    {
-        if (nums[i] != nums[j])
-            return false;
-    }
-    
-    return true;
+    second = reverseSecondHalf(second);
+    return result;
 }
