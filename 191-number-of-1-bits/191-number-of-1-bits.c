2fc7a5e82237e27a7e4643dc794bccf8a02507f1
Arun Raveendran <arunr4957@gmail.com>
2022-03-28 14:56:00 +0530

    Time: 7 ms (11.51%), Space: 5.5 MB (54.60%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.c
+++ 191-number-of-1-bits/191-number-of-1-bits.c
@@ -1,11 +1,10 @@
 int hammingWeight(uint32_t n) {
-    int pos;
     int count = 0 ;
     
-    for (pos = 31; pos >= 0; --pos)
+    while (n)
     {
-        if ((n >> pos) & 1)
-            count++;
+        n &= (n - 1);
+        count++;
     }
     
     return count;
