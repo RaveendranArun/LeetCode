7d29713ef4d8ee41654025b70ac07c317717e3bb
Arun Raveendran <arunr4957@gmail.com>
2022-06-14 22:02:55 +0530

    Time: 127 ms (7.22%), Space: 7.5 MB (66.92%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.c
@@ -0,0 +1,20 @@
+
+
+void duplicateZeros(int* arr, int arrSize){
+    int i = 0, j;
+    
+    while (i < arrSize)
+    {
+        if (arr[i] == 0)
+        {
+            i++;
+            for (j = arrSize-1; j > i; --j)
+            {
+                arr[j] = arr[j-1];
+            }
+            arr[j] = 0;
+            //arrSize += 1;
+        } 
+        i++;
+    }
+}
